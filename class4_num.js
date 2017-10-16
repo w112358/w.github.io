0adc085e8aa9ce15eae0b255d8acdf12c004f360
zqww <wolf112358@hotmail.com>
2017-10-16 08:35:43 -0700

    homework4

---
+++ class4_num.js
@@ -0,0 +1,16 @@
+<script>
+
+function plusTen(num){
+  return num + 10;
+}
+
+function minsTen(num){
+  return num - 10;
+}
+
+var numbers = {
+  plus10: plusTen,
+  mins10: minsTen
+};
+
+</script>
